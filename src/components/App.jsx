573032cb27b030586d5504ebfe808534bc5a4af3
Ernes Nafieiev <demodekoz@gmail.com>
2023-01-23 00:09:40 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Form } from 'components/ContactForm/Form';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
